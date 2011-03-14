0000bb48bf50a02177b43f6ee4c3155522df8b7b
Adam Bucketz <adam@adambuchweitz.com>
2011-03-13 21:19:28 -0500

    v1.1.3 Hotfix
    Fixed a "bug" where reseting the transform of a text object when inserting into a group would revert it back to it's double size.
    This was affecting tableview and button classes.

--- crawlspaceLib.lua
+++ crawlspaceLib.lua
@@ -1,7 +1,7 @@
 
         --[[ ########## Crawl Space Library ########## ]--
 
-    Version 1.1.2
+    Version 1.1.3
 
     Written and supported by Adam Buchweitz and Crawl Space Games
 
@@ -320,8 +320,8 @@ local crawlspaceInsert = function(...)
     local t = {...}
     local b, reset = 0, nil
     if type(t[#t]) == "boolean" then b = 1; reset = t[#t] end
-    if type(t[2]) == "number" then for i=3, #t-b do t[1]:cachedInsert(t[2],t[i],reset) end
-    else for i=2, #t-b do t[1]:cachedInsert(t[i],reset) end
+    if type(t[2]) == "number" then for i=3, #t-b do t[1]:cachedInsert(t[2],t[i],reset); if reset and t[i].text then t[i].xScale, t[i].yScale=.5,.5 end end
+    else for i=2, #t-b do t[1]:cachedInsert(t[i],reset); if reset and t[i].text then t[i].xScale, t[i].yScale=.5,.5 end end
     end
 end
 local cachedNewGroup = display.newGroup
@@ -1082,7 +1082,7 @@ local welcome = function()
     local print = cachedPrint
     print("\n\n\nYou can disable this welcome message by setting the 'showIntro' variable to\nfalse in crawlspacelib.lua")
     print("\n\n")
-    print("CrawlSpaceLib v1.1.2\n")
+    print("CrawlSpaceLib v1.1.3\n")
     print("Welcome to the CrawlSpace Library!\n\n\t    A lot of work has gone into making this very powerful while also\n\tkeeping it very, very easy to use. With that being said, we will happily\n\taccept your donation! Try the library out, see if it saves you any time,\n\teffort, or headaches, and even if you don't decide to donate, I'd still\n\tlove to hear about your experience.")
     print("\n\t    Take a peek at crawlspacelib.lua for detailed instructions on\n\thow to use the library, or just take advantage of its features passivly,\n\tas many are simple enhancements to existing CoronaSDK functionality.\n\tAlternatively, you may run the crawlspacelib.listFeatures() method to\n\tget a quick list of everything included.")
     print("\n\n\tDonate: http://www.crawlspacegames.com/crawl-space-corona-sdk-library/\n\n\tEmail me: adam@crawlspacegames.com")
