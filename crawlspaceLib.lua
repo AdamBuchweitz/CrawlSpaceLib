6e50b16d508dc7a0e95309e11d79af5b2f40426a
Adam Bucketz <adam@adambuchweitz.com>
2011-04-19 02:18:33 -0500

    Debugger half functional - prints, fps, and memory usage

--- crawlspaceLib.lua
+++ crawlspaceLib.lua
@@ -1111,21 +1111,21 @@ print = function( ... )
         elseif #{...} > 1 then cache.print("\nOutput mutiple: ", ...)
         else cache.print("\nOutput "..type(a).." :: ", a or "") end
     else
-        if CSL.debugWindow then
+        if CSL.printWindow then
             if #{...} > 1 then
                 for k,v in pairs(a) do
                     printYpos = printYpos + 12
                     printObj = display.newText(tostring(v), 0, printYpos, native.systemFont, 10, "tl")
                     printObj:setTextColor(255, 255, 255)
-                    CSL.debugWindow:insert(printObj)
-                    CSL.debugWindow.y = CSL.debugWindow.y - 12
+                    CSL.printWindow:insert(printObj)
+                    CSL.printWindow.y = CSL.printWindow.y - 12
                 end
             else
                 printYpos = printYpos + 12
                 printObj = display.newText(tostring(a), 0, printYpos, native.systemFont, 10, "tl")
                 printObj:setTextColor(255, 255, 255)
-                CSL.debugWindow:insert(printObj)
-                CSL.debugWindow.y = CSL.debugWindow.y - 12
+                CSL.printWindow:insert(printObj)
+                CSL.printWindow.y = CSL.printWindow.y - 12
             end
         end
     end
@@ -1201,7 +1201,19 @@ end
 
 if showIntro then welcome() elseif startupTips then showTip() end
 
+
+
+            --[[ ############################################### ]]--
+            --[[ ####################       #################### ]]--
+            --[[ #################### BETA! #################### ]]--
+            --[[ ####################       #################### ]]--
+            --[[ ############################################### ]]--
+
+
+            --[[ ########## Enable ########## ]]--
 cache.require = require
+
+            --[[ ########## Open Feint Expansion ########## ]]--
 local achievements, leaderboards, openfeint
 local enableOF = function( params )
     openfeint = cache.require("openfeint")
@@ -1213,7 +1225,7 @@ local enableOF = function( params )
     end
     openfeint.init(params.productKey,params.productSecret,params.displayName,params.appId)
     --if system.pathForFile("feint.lua", system.ResourceDirectory) then local feint = require("feint"); achievements, leaderboards = feint.achievements, feint.leaderboards end
-    local feint = cache.require("feint")
+    local feint = cache.require("feint") -- external library
     achievements, leaderboards = feint.achievements, feint.leaderboards
 end
 Achieve = function( achievement )
@@ -1249,17 +1261,283 @@ HighScore = function( board, score, display )
     --openfeint.setHighScore( { leaderboardID=boards[board], score=curBest, displayText=bestScore.text} )
 end
 
+            --[[ ########## Flurry Expansion ########## ]]--
 local enableFlurry = function( id )
     if not id then print("Please supply a valid app ID")
     else analytics.init(id) end
 end
+Log = function()
+end
 
-local libraryMethods = { openfeint = enableOF, analytics = enableFlurry}
+
+            --[[ ########## Random help ########## ]]--
+-- Generate randomseed and pull the first three numbers out
+math.randomseed(system.getTimer())
+math.random(); math.random(); math.random()
+
+            --[[ ########## Set up the platform table ########## ]]--
+platform = {}
+local name = string.lower(system.getInfo("platformName"))
+if name     == "android"   then platform.android = true
+elseif name == "iphone os" then platform.ios     = true
+elseif name == "win"       then platform.win     = true
+elseif name == "mac os x"  then platform.mac     = true end
+platform.name = name
+
+
+
+local prevTime = 0;
+local maxSavedFps = 30;
+local function minElement(table)
+    local min = 10000;
+    for i = 1, #table do if(table[i] < min) then min = table[i]; end end
+    return min;
+end
+
+local function getLabelUpdater(self)
+end
+
+local fpsDialog
+local showFPS = function()
+    local makeFpsDialog = function()
+        local fps = display.newGroup()
+
+        fps.background = display.newRect(screenX, screenY, screenWidth, 50)
+        fps.background:setFillColor(0,0,0)
+
+        fps.memory = display.newText("0/10", centerX, screenY, "helvetica", 13, "tc")
+        fps.memory:setTextColor(255,255,255)
+
+        fps.framerate = display.newText("0", centerX, fps.memory.contentHeight+5, "Helvetica", 16, "tc")
+        fps.framerate:setTextColor(255,255,255)
+
+        fps:insert(fps.background, fps.memory, fps.framerate)
+        fps.alpha = .5
+
+        transition.from(fps, {time=200, y=screenY-50})
+
+        return fps;
+    end
+
+    local lastFps = {}
+    local lastFpsCounter = 1
+    local updateFPS = function( event )
+        local curTime = system.getTimer();
+        local dt = curTime - prevTime;
+        prevTime = curTime;
+
+        local fps = math.floor(1000/dt);
+
+        lastFps[lastFpsCounter] = fps;
+        lastFpsCounter = lastFpsCounter + 1;
+        if(lastFpsCounter > maxSavedFps) then lastFpsCounter = 1; end
+        local minLastFps = minElement(lastFps);
+
+        fpsDialog.framerate.text = "FPS: "..fps.." (min: "..minLastFps..")";
+
+        local mem = math.ceil(system.getInfo("textureMemoryUsed")*.0001)*.01
+        fpsDialog.memory.text = "Mem: "..mem.." mb";
+    end
+
+    if fpsDialog then
+        if fpsDialog.y == screenY then
+            transition.to(fpsDialog, {time=200, y=screenY-50})
+            Runtime:removeEventListener("enterFrame", updateFPS)
+        else
+            transition.to(fpsDialog, {time=200, y=screenY})
+            Runtime:addEventListener("enterFrame", updateFPS)
+        end
+    else
+        fpsDialog = makeFpsDialog()
+        Runtime:addEventListener("enterFrame", updateFPS)
+    end
+end
+
+local showVars = function()
+    print("vars")
+end
+
+local showPrints = function()
+    if CSL.printWindow then
+        if CSL.printWindow.isVisible then CSL.printWindow.isVisible = false
+        else CSL.printWindow.isVisible = true end
+    else
+        printYpos = screenY + 20
+        printWindow = display.newGroup()
+        printWindow.x, printWindow.y = screenX + 10, printYpos
+        printWindow:setReferencePoint(display.bl)
+        printWindow:addEventListener("touch", printWindow)
+        printWindow.delta = printYpos
+        printWindow.touch = function( self, event )
+            if event.phase == "began" then
+                display.getCurrentStage():setFocus( self )
+            elseif event.phase == "moved" then
+                printWindow.y = printWindow.delta + event.y-event.yStart
+            elseif event.phase == "ended" then
+                printWindow.delta = printWindow.y
+                if printWindow.y + printWindow.contentHeight < screenY+20 then
+                    transition.to(printWindow, {y=screenY+20-printWindow.contentHeight, time=100, transitioning=easing.inOutQuad})
+                    printWindow.delta = screenY+20-printWindow.contentHeight
+                elseif printWindow.y > screenY+screenHeight-100 then
+                    transition.to(printWindow, {y=screenY+screenHeight-100, time=100, transitioning=easing.inOutQuad})
+                    printWindow.delta = screenY+screenHeight-100
+                end
+                display.getCurrentStage():setFocus( nil )
+            end
+        end
+        printWindow:fadeIn()
+        CSL.printWindow = printWindow
+        print(":: PRINT ::")
+    end
+end
+
+
+local debugUI
+local showDialog = function()
+    local makeDebugUI = function()
+        local g = display.newGroup()
+
+        -- background glow
+        local b
+        for i=1, 10 do
+            b = display.newCircle( centerX, centerY, 111 - i )
+            b.alpha = i * 0.03
+            g:insert(b)
+        end
+
+        -- background circle
+        local oc = display.newCircle( centerX, centerY, 100 )
+        oc:setFillColor( 0, 0, 0 )
+        oc.alpha = 0.85
+
+        -- inner circle
+        local ic = display.newCircle( centerX, centerY, 30 )
+        ic:setFillColor(0,0,0,0)
+        ic.alpha = 0.25
+        ic.strokeWidth = 1
+        ic:setStrokeColor(255,255,255)
+
+        -- make glows
+        local makeGlow = function()
+            local g = display.newGroup()
+            for i=1, 10 do
+                b = display.newCircle( 0, 0, 41 - i*3 )
+                b.alpha = i * 0.015
+                g:insert(b)
+            end
+            return g
+        end
+        -- lines and glow groups
+        local lineGroup, glowGroup = display.newGroup(), display.newGroup()
+        lineGroup.x, lineGroup.y = centerX, centerY
+        lineGroup.rotation = 45
+        glowGroup.x, glowGroup.y = centerX, centerY
+
+        local side = {0,-1,0,1}
+        local side2 = {0,1,0,-1}
+        local abs, l, glow = math.abs
+        for i=1, 4 do
+            l = display.newLine(30, 0, 100, 0)
+            l.x, l.y = 30*side[i], 30*side[5-i]
+            l.alpha = 0.25
+            l.rotation = 90*i
+            glow = makeGlow()
+            glow.x, glow.y = 65*side2[i], 65*side2[5-i]
+            glow.xScale, glow.yScale = 1-0.4*abs(side[i]), 1-0.4*abs(side[5-i])
+            glow.alpha = 0
+            glow.on = false
+            glowGroup:insert(glow)
+            lineGroup:insert(l)
+        end
+
+        local checkPos = function( event )
+            local newState = 0
+            if event.x < centerX - 40 then
+                newState = 4
+            elseif event.x > centerX + 40 then
+                newState = 2
+            end
+            if event.y > centerY + 40 then
+                newState = 3
+            elseif event.y < centerY - 40 then
+                newState = 1
+            end
+            if newState ~= debugUI.state then
+                --if debugUI.state > 0 then
+                    --glowGroup[debugUI.state]:fadeOut()
+                --end
+            end
+            if newState > 0 then
+                local glo = glowGroup[newState]
+                if glo.on then
+                    glo:fadeOut()
+                    glo.on = false
+                else
+                    glo:fadeIn()
+                    glo.on = true
+                end
+            end
+            debugUI.state = newState
+        end
+        g.functions = { showFPS, nil, showVars, showPrints }
+        g.touch = function( self, event )
+            local phase = event.phase
+            if phase == "began" then
+                checkPos(event)
+                display.getCurrentStage():setFocus( self )
+            elseif phase == "moved" then
+                checkPos(event)
+            elseif phase == "ended" then
+                display.getCurrentStage():setFocus( nil )
+                if g.state > 0 then
+                    g.functions[g.state]()
+                elseif g.canClose then
+                    g:fadeOut()
+                else
+                    g.canClose = true
+                end
+            end
+            return true
+        end
+
+        g:addEventListener("touch", g)
+        g:insert(oc, ic, lineGroup, glowGroup)
+
+
+        return g
+    end
+
+    if not debugUI then
+        debugUI = makeDebugUI()
+    end
+    debugUI.canClose = false
+    debugUI.state = 0
+    debugUI:fadeIn()
+
+    return true
+end
+
+
+local debugTimer
+local debugger = function()
+    local listener = function( event )
+        local phase = event.phase
+        if phase == "began" then
+            debugTimer = timer.performWithDelay(1000, showDialog )
+        elseif phase == "ended" then timer.cancel(debugTimer) end
+    end
+    Runtime:addEventListener("touch", listener)
+end
+
+
+local libraryMethods = { openfeint = enableOF, analytics = enableFlurry, debug = debugger }
 local libraryWhitelist = {"audio"}
 local checkWhitelist = function(toCheck)
     for i,v in ipairs(libraryWhitelist) do if toCheck == v then return true end end
     return false
 end
+
+            --[[ ########## The actual method ########## ]]--
 Enable = function(library, params)
     local l
     if package.preload[ library ] then l=cache.require(library)
@@ -1272,168 +1550,5 @@ Enable = function(library, params)
     end
 end
 if simulator then require = Enable end
-
-
--- Generate randomseed and pull the first three numbers out
-math.randomseed(system.getTimer())
-math.random(); math.random(); math.random()
-
-platform = {}
-local name = string.lower(system.getInfo("platformName"))
-if name     == "android"   then platform.android = true
-elseif name == "iphone os" then platform.ios     = true
-elseif name == "win"       then platform.win     = true
-elseif name == "mac os x"  then platform.mac     = true end
-platform.name = name
-
-local debugUI
-local setupDebugger = function()
-    if not debugUI then
-        debugUI = display.newGroup()
-    end
-    local g = debugUI
-    g.state = 0
-
-    -- background glow
-    local b
-    for i=1, 10 do
-        b = display.newCircle( centerX, centerY, 111 - i )
-        b.alpha = i * 0.03
-        g:insert(b)
-    end
-
-    -- background circle
-    local oc = display.newCircle( centerX, centerY, 100 )
-    oc:setFillColor( 0, 0, 0 )
-    oc.alpha = 0.85
-
-    -- inner circle
-    local ic = display.newCircle( centerX, centerY, 30 )
-    ic:setFillColor(0,0,0,0)
-    ic.alpha = 0.25
-    ic.strokeWidth = 1
-    ic:setStrokeColor(255,255,255)
-
-    -- make glows
-    local makeGlow = function()
-        local g = display.newGroup()
-        for i=1, 10 do
-            b = display.newCircle( 0, 0, 41 - i*3 )
-            b.alpha = i * 0.015
-            g:insert(b)
-        end
-        return g
-    end
-
-    -- lines and glow groups
-    local lineGroup, glowGroup = display.newGroup(), display.newGroup()
-    lineGroup.x, lineGroup.y = centerX, centerY
-    lineGroup.rotation = 45
-    glowGroup.x, glowGroup.y = centerX, centerY
-
-    local side = {0,-1,0,1}
-    local side2 = {0,1,0,-1}
-    local abs, l, glow = math.abs
-    for i=1, 4 do
-        l = display.newLine(30, 0, 100, 0)
-        l.x, l.y = 30*side[i], 30*side[5-i]
-        l.alpha = 0.25
-        l.rotation = 90*i
-        glow = makeGlow()
-        glow.x, glow.y = 65*side2[i], 65*side2[5-i]
-        glow.xScale, glow.yScale = 1-0.4*abs(side[i]), 1-0.4*abs(side[5-i])
-        glow.alpha = 0
-        glowGroup:insert(glow)
-        lineGroup:insert(l)
-    end
-
-    local checkPos = function( event )
-        local newState = 0
-        if event.x < centerX - 40 then
-            newState = 4
-        elseif event.x > centerX + 40 then
-            newState = 2
-        end
-        if event.y > centerY + 40 then
-            newState = 3
-        elseif event.y < centerY - 40 then
-            newState = 1
-        end
-        if newState ~= g.state then
-            if g.state > 0 then
-                glowGroup[g.state]:fadeOut()
-            end
-            if newState > 0 then
-                glowGroup[newState]:fadeIn()
-            end
-            g.state = newState
-        end
-    end
-    g.touch = function( self, event )
-        local phase = event.phase
-        if phase == "began" then
-            checkPos(event)
-            display.getCurrentStage():setFocus( self )
-        elseif phase == "moved" then
-            checkPos(event)
-        elseif phase == "ended" then
-            display.getCurrentStage():setFocus( nil )
-            if g.state == 0 then
-                g:fadeOut()
-            end
-        end
-        return true
-    end
-
-    g:addEventListener("touch", g)
-    g:insert(oc, ic, lineGroup, glowGroup)
-    g:fadeIn()
-    return g
-end
-
-local debugTimer= nil
-local set = function()
-    if CSL.debugWindow then
-        if CSL.debugWindow.isVisible then CSL.debugWindow.isVisible = false
-        else CSL.debugWindow.isVisible = true end
-    else
-        printYpos = screenY + 20
-        debugWindow = display.newGroup()
-        debugWindow.x, debugWindow.y = screenX + 10, printYpos
-        debugWindow:setReferencePoint(display.bl)
-        debugWindow:addEventListener("touch", debugWindow)
-        debugWindow.delta = printYpos
-        debugWindow.touch = function( self, event )
-            if event.phase == "began" then
-                display.getCurrentStage():setFocus( self )
-            elseif event.phase == "moved" then
-                debugWindow.y = debugWindow.delta + event.y-event.yStart
-            elseif event.phase == "ended" then
-                debugWindow.delta = debugWindow.y
-                if debugWindow.y + debugWindow.contentHeight < screenY+20 then
-                    transition.to(debugWindow, {y=screenY+20-debugWindow.contentHeight, time=100, transitioning=easing.inOutQuad})
-                    debugWindow.delta = screenY+20-debugWindow.contentHeight
-                elseif debugWindow.y > screenY+screenHeight-100 then
-                    transition.to(debugWindow, {y=screenY+screenHeight-100, time=100, transitioning=easing.inOutQuad})
-                    debugWindow.delta = screenY+screenHeight-100
-                end
-                display.getCurrentStage():setFocus( nil )
-            end
-        end
-        CSL.debugWindow = debugWindow
-        print(":: PRINT ::")
-    end
-
-    local debugSelector = setupDebugger()
-end
-
-CSL.printDebugger = function( event )
-    local event = event
-    if event.phase == "began" then
-        debugTimer = timer.performWithDelay(1000, set)
-    elseif event.phase == "ended" then timer.cancel(debugTimer) end
-end
-
-if debug then Runtime:addEventListener("touch", CSL.printDebugger) end
 
 return CSL
