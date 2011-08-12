9358a78cf4ed3634787cb734c2d16681322e8c81
Adam Buchweitz <adam@adambuchweitz.com>
2011-08-12 11:52:14 -0500

    OpenFeint stuff to make it work on simulator

--- crawlspaceLib.lua
+++ crawlspaceLib.lua
@@ -1269,16 +1269,18 @@ cache.require = require
             --[[ ########## Open Feint Expansion ########## ]]--
 local achievements, leaderboards, gameNetwork
 local enableOF = function( params )
-    gameNetwork = cache.require("gameNetwork")
-    if not params then print("Please provide me with Open Feint info"); return false else
-        if not params.productKey    then print("Missing Open Feint product key") end
-        if not params.productSecret then print("Missing Open Feint product secret") end
-        if not params.displayName   then print("Missing Open Feint display name") end
-        if not params.appId         then print("Missing Open Feint app ID") end
+    if not simulator then
+        gameNetwork = cache.require("gameNetwork")
+        if not params then print("Please provide me with Open Feint info"); return false else
+            if not params.productKey    then print("Missing Open Feint product key") end
+            if not params.productSecret then print("Missing Open Feint product secret") end
+            if not params.displayName   then print("Missing Open Feint display name") end
+            if not params.appId         then print("Missing Open Feint app ID") end
+        end
+        openfeint.launchDashboard = gameNetwork.show
+        gameNetwork.init("openfeint",params.productKey,params.productSecret,params.displayName,params.appId)
     end
     openfeint = {}
-    openfeint.launchDashboard = gameNetwork.show
-    gameNetwork.init("openfeint",params.productKey,params.productSecret,params.displayName,params.appId)
     if system.pathForFile("feint.lua", system.ResourceDirectory) then local feint = require("feint"); achievements, leaderboards = feint.achievements, feint.leaderboards; openfeint.leaderboards, openfeint.achievements = leaderboards, achievements end
     --local feint = cache.require("feint") -- external library
     --achievements, leaderboards = feint.achievements, feint.leaderboards
@@ -1287,16 +1289,18 @@ end
 
 Achieve = function( achievement )
     --if not package.loaded["gameNetwork"] then print("Please Enable gameNetwork before attempting to unlock and achievement."); return false end
-    if tonum(achievement) then
-        if #tostring(achievement) ~= 6 then print("Invalid achievement number")
-        else gameNetwork.request("unlockAchievement",tostring(achievement)) end
-    else
-        if achievements then
-            if achievements[achievement] then gameNetwork.request("unlockAchievement",tostring(achievements[achievement]))
-            else print("Cannot find that achievement") end
+    if not simulator then
+        if tonum(achievement) then
+            if #tostring(achievement) ~= 6 then print("Invalid achievement number")
+            else gameNetwork.request("unlockAchievement",tostring(achievement)) end
         else
-            if _G.achievements and _G.achievements[achievement] then gameNetwork.request("unlockAchievement",tostring(_G.achievements[achievement]))
-            else print("Cannot find that achievement") end
+            if achievements then
+                if achievements[achievement] then gameNetwork.request("unlockAchievement",tostring(achievements[achievement]))
+                else print("Cannot find that achievement") end
+            else
+                if _G.achievements and _G.achievements[achievement] then gameNetwork.request("unlockAchievement",tostring(_G.achievements[achievement]))
+                else print("Cannot find that achievement") end
+            end
         end
     end
 end
@@ -1315,7 +1319,9 @@ HighScore = function( board, score, display )
             else print("Cannot find that leaderboard"); return false end
         end
     end
-    gameNetwork.request("setHighScore", { leaderboardID=tostring(board), score=tonum(score), displayText=tostring(display)})
+    if not simulator then
+        gameNetwork.request("setHighScore", { leaderboardID=tostring(board), score=tonum(score), displayText=tostring(display)})
+    end
 end
 
             --[[ ########## Flurry Expansion ########## ]]--
